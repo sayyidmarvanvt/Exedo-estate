ed7a080887c42999fe8b452c8c9e5db1f2497620
Sayyid Marvan <sayyidmarvanvt@gmail.com>
2024-05-12 15:39:49 +0530

    changed error handling in signup

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -6,14 +6,18 @@ import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ username, email, password: hashedPassword });
 
   try {
+    const existingUser = await User.findOne({ email,username });
+    if (existingUser) {
+      return next(errorHandler(400, "Already exists"));
+    }
+    const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save(); //save into the model
     res.status(201).json("User created successfully");
   } catch (error) {
+    next(errorHandler(500,"Something wrong"));
     // next(errorHandler(550,'error from functiom')) //created err
-    next(error);
     // res.status(500).json(error.message)//sending duplicate error or errors to user
   }
 };
@@ -27,7 +31,7 @@ export const signin = async (req, res, next) => {
     if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
-    //pass is used because above password called in const 
+    //pass is used because above password called in const
     res
       .cookie("access_token", token, { httpOnly: true })
       .status(200)
