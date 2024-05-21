dc21de25ce910ba0fa4131b83ea8dc2abf39148b
Sayyid Marvan <sayyidmarvanvt@gmail.com>
2024-05-21 15:09:38 +0530

    change cookie expiry to 7 days

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -16,7 +16,7 @@ export const signup = async (req, res, next) => {
     console.log(newUser);
     const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = newUser._doc;
-    const expiryDate = new Date(Date.now() + 36000000);
+    const expiryDate = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); // 7 days expiry
     await newUser.save(); // Save user into the database
     res
       .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
@@ -24,7 +24,7 @@ export const signup = async (req, res, next) => {
       .json(rest); // Send user data without password
   } catch (error) {
     next(error);
-    // next(errorHandler(550,'error from functiom')) //created err
+    // next(errorHandler(550,'error from function')) //created err
     // res.status(500).json(error.message)//sending duplicate error or errors to user
   }
 };
@@ -33,17 +33,17 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) return next(errorHandler(404, "user not found"));
+    if (!validUser) return next(errorHandler(404, "User not found"));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
-    //pass is used because above password called in const
-    const expiryDate = new Date(Date.now() + 36000000);
+    // pass is used because above password called in const
+    const expiryDate = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); // 7 days expiry
     res
       .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
       .status(200)
-      .json(rest); //created cookie protected (httpOnly) and sending without password(rest)
+      .json(rest); // created cookie protected (httpOnly) and sending without password (rest)
   } catch (error) {
     next(error);
   }
@@ -55,7 +55,7 @@ export const google = async (req, res, next) => {
     if (user) {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = user._doc;
-      const expiryDate = new Date(Date.now() + 36000000);
+      const expiryDate = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); // 7 days expiry
       res
         .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
         .status(200)
@@ -74,7 +74,7 @@ export const google = async (req, res, next) => {
       await newUser.save();
       const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = newUser._doc;
-      const expiryDate = new Date(Date.now() + 3600000);
+      const expiryDate = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); // 7 days expiry
       res
         .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
         .status(200)
