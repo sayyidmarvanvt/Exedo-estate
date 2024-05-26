d91b13d2158e284fa7685eccaead8545426a175a
Sayyid Marvan <sayyidmarvanvt@gmail.com>
2024-05-26 13:01:06 +0530

    add contact landrord to the listing

---
+++ client/src/components/Contact.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Contact({ listing }) {
+  const [landlord, setLandlord] = useState(null);
+  const [message,setMesssage]=useState('')
+  const onChange=(e)=>{
+    setMesssage(e.target.value)
+  }
+
+
+  useEffect(() => {
+    const fetchLandlord = async () => {
+      try {
+        const res = await fetch(`/api/user/contact/${listing.userRef}`);
+        const data = await res.json();
+        setLandlord(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchLandlord();
+  }, [listing.userRef]);
+  console.log(landlord);
+  return (
+    <>
+      {landlord && (
+        <div className="flex flex-col gap-2">
+          <p>
+            Contact <span className="font-semibold">{landlord.username}</span> for <span className="font-semibold">{listing.name.toLowerCase()}</span>
+          </p>
+          <textarea name="message" id="message" rows="2" value={message} onChange={onChange}  placeholder="Enter your message" className="w-full p-3 border rounded-lg"></textarea>
+          <Link to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`} className='bg-slate-700 text-white text-center p-3 uppercasevrounded-lg hover:opacity-95'>Send Message</Link>
+        </div>
+      )}
+    </>
+  );
+}
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -59,3 +59,17 @@ try {
     return next(errorHandler(401,"You can only view your own listings!"))
   }
 }
+
+export const contactUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return next(errorHandler(404, "User not found!"));
+    }
+    const { password:pass, ...rest } = user._doc;
+    res.status(200).json(rest);
+    console.log(rest);
+  } catch (error) {
+    next(error);
+  }
+};
