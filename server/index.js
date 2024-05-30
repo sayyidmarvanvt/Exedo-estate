0c54ff8b37f6e0da67c74830964061b0bcdab040
Sayyid Marvan <sayyidmarvanvt@gmail.com>
2024-05-30 15:44:14 +0530

    fixed

--- server/index.js
+++ server/index.js
@@ -32,12 +32,59 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
-app.use(express.static(path.join(__dirname, 'client/dist')));
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js"
+import cookieParser from "cookie-parser";
+import path from "path"
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+const __dirname=path.resolve()
+const app = express();
+app.use(express.json()); //allow json as input of server
+app.use(cookieParser())
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+// app.get('/test',(req,res)=>{res.send("hello world")})
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, '..', 'client')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(process.cwd(), 'client/dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+});
+
+console.log(__dirname);
+//error middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
 
 
+console.log(__dirname);
 //error middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
