cd2f191349174585866434f2c3ded119de2e6c9e
Sayyid Marvan <sayyidmarvanvt@gmail.com>
2024-06-01 17:17:45 +0530

    fixed

--- server/index.js
+++ server/index.js
@@ -13,16 +13,14 @@ const __dirname = path.resolve();
 const app = express();
 const PORT = 3000;
 
-const allowedOrigins = ["http://localhost:5173", "https://real-estate-usfd.onrender.com"];
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://real-estate-usfd.onrender.com"
+];
+
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  origin: allowedOrigins,
   credentials: true,
 }));
 
