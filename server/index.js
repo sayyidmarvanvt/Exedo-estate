1bcb9ad22173fcc8b2e955838c23f6f7fa0ffe5d
Sayyid Marvan <sayyidmarvanvt@gmail.com>
2024-06-01 16:59:10 +0530

    add cors

--- server/index.js
+++ server/index.js
@@ -5,9 +5,22 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
-import path from "path";
+import cors from "cors";
 
 dotenv.config();
+
+const allowedOrigins = ["http://localhost:5173", "https://real-estate-usfd.onrender.com"];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+}));
 
 mongoose
   .connect(process.env.MONGO)
@@ -34,11 +47,11 @@ app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
 // Serve static files from the client build directory
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 // Error middleware
 app.use((err, req, res, next) => {
